37fe95ef43a81f2e3280e18d00235f71dc4ad7f7
johncraik <jcraik23@gmail.com>
2025-08-13 19:39:00 +0100

    Initialize ASP.NET Core MVC project with Identity authentication and SQLite support

---
+++ VPN-Portal/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VPN_Portal.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
